bd13d600c97d456a7bbe13140188a50c6a8c18b8
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-09-30 13:26:47 +0300

    add some comments

--- learn.javaScript.screencast/asyncWorking/server.js
+++ learn.javaScript.screencast/asyncWorking/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
+/*
+Обязвтельно орабатывать ошибки
+не работает try-catch => cb(err)
+есть соглашение о том что: первый агрумент функции оработчика всегда является ошибкой:
+function cb(err,...)
+при ошибке: cb(err);
+без ошибки: cb(err,...);
+ */
 http.createServer(function (req, res) {
 	var info;
 	if (req.url === '/') {
@@ -8,10 +16,10 @@ http.createServer(function (req, res) {
 			if (err) {
 				console.log(err);
 				res.statusCode = 500;
-				res.end("На сервере произошкла ошибка!")
+				res.end("Error on server!");
 				return;
 			}
 			res.end(info);
 		});
 	} else {/* 404 */}
-}).listen(3000);
+}).listen(3000); // слушает 3000 порт (Доступ к странице 127.0.0.1:3000)
