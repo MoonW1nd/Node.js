9631b8cdb20acce946cf836d6bd70ffa9ceff0f9
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-12-09 20:51:44 +0300

    testing simple routing

--- Learn-Node-master/routes/index.js
+++ Learn-Node-master/routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = {name: 'Alexander', age: 26, cool: true };
+  // res.send('Hey! It works!');
+  //   res.json(wes);
+    res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // res.send('Work here!');
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
