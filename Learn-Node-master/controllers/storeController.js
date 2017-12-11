46018482c50903b9d36edecee5bfaf119de72bd4
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-12-11 23:57:10 +0300

    simple Middleware implementation

--- Learn-Node-master/controllers/storeController.js
+++ Learn-Node-master/controllers/storeController.js
@@ -1,3 +1,9 @@
+exports.myMiddleware = (req, res, next) => {
+	req.messageMiddleware = 'Hello from Middleware';
+	next();
+};
+
 exports.homePage = (req, res) => {
+	console.log(req.messageMiddleware);
 	res.render('hello', {name: 'wes', dog: 'Puppy'});
 };
