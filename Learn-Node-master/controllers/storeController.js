11af962a73b488af06ce5c63b696d3f3f859ab06
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-12-12 17:31:31 +0300

    Create add page (routing, pug)

--- Learn-Node-master/controllers/storeController.js
+++ Learn-Node-master/controllers/storeController.js
@@ -7,3 +7,12 @@ exports.homePage = (req, res) => {
 	console.log(req.messageMiddleware);
 	res.render('hello', {name: 'wes', dog: 'Puppy'});
 };
+
+exports.addStore = (req, res) => {
+	res.render('editStore', { title: 'Add Store'});
+};
+
+exports.createStore = (req, res) => {
+	console.log(req.body);
+	res.json(req.body);
+};
