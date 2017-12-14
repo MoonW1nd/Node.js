6d91456e968ffd31f0b20d1dba98add2f5d93fd7
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-12-14 14:27:44 +0300

    Create Stores pages, shape and mixin (first read from db)

--- Learn-Node-master/controllers/storeController.js
+++ Learn-Node-master/controllers/storeController.js
@@ -3,22 +3,28 @@ const Store = mongoose.model('Store');
 
 
 
-exports.myMiddleware = (req, res, next) => {
-	req.messageMiddleware = 'Hello from Middleware';
-	next();
-};
-
-exports.homePage = (req, res) => {
-	console.log(req.messageMiddleware);
-	res.render('hello', {name: 'wes', dog: 'Puppy'});
-};
+// exports.myMiddleware = (req, res, next) => {
+// 	req.messageMiddleware = 'Hello from Middleware';
+// 	next();
+// };
+//
+// exports.homePage = (req, res) => {
+// 	console.log(req.messageMiddleware);
+// 	res.render('hello', {name: 'wes', dog: 'Puppy'});
+// };
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store'});
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
-	res.redirect('/');
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully Created ${store.name}. Care to leave in review?`);
+	res.redirect(`/store/${store.slug}`);
+};
+
+
+exports.getStores = async (req, res) => {
+	const stores = await Store.find();
+	res.render('stores', { title: 'Stores', stores });
 };
