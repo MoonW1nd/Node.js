1a6ca4d73f26610384b83e7971bce39a328ce258
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-12-16 15:29:18 +0300

    implements update data store

--- Learn-Node-master/controllers/storeController.js
+++ Learn-Node-master/controllers/storeController.js
@@ -28,3 +28,20 @@ exports.getStores = async (req, res) => {
 	const stores = await Store.find();
 	res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+	//TODO:[A.Ivankov] 1. find the store given id
+	const store = await Store.findOne({_id: req.params.id});
+	//TODO:[A.Ivankov] 2. confirm they are the owner of the store
+	res.render('editStore', { title: `Edit ${ store.name }`, store })
+	//TODO:[A.Ivankov] 3. render out edit form so the user can update their store
+};
+
+exports.updateStore = async (req, res) => {
+	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true,
+		runValidators: true
+	}).exec();
+	req.flash('success', `Success update <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`);
+	res.redirect(`store/${store.id}/edit`);
+};
