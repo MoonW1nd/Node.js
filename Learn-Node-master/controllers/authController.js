3ce002a225d1cdf531fa27b4d22212e6ca0c1e47
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-01-08 00:44:20 +0300

    Start implementation remind password functionality

--- Learn-Node-master/controllers/authController.js
+++ Learn-Node-master/controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
 	failureRedirect: '/login',
@@ -20,4 +23,18 @@ exports.isLoggedIn = (req, res, next) => {
 	}
 	req.flash('error', 'Ooops you must be logged in to do this!');
 	res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+	//see if a user with that a email exist
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) {
+		req.flash('error', 'No account with that email exists.')
+		return res.redirect('/login')
+	}
+	//set reset tokens and expiry on their account
+	user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+	user.resetPasswordExpires = Date.now() + 3600000; //1 hour form now
+	await user.save();
+	
 };
