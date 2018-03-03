686bb8662add82a80f786450ca237885a62c16de
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-03-03 14:06:39 +0300

    Start like implementation(35)

--- Learn-Node-master/models/User.js
+++ Learn-Node-master/models/User.js
@@ -21,7 +21,10 @@ const userSchema = new Schema({
 		required: 'Please supply a name'
 	},
 	resetPasswordToken: String,
-	resetPasswordExpires: Date
+	resetPasswordExpires: Date,
+	hearts: [
+		{ type: mongoose.Schema.ObjectId, ref: 'store' }
+	]
 });
 
 userSchema.virtual('gravatar').get(function () {
